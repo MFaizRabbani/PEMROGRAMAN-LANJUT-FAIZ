7db6611484ae439ff51dd6ad35b0b19e297eea0e
MFaizRabbani <MFaizRabbani>
2021-04-06 22:56:56 +0700

    Arrays

---
+++ src/src/arrays/Enhanced.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Enhanced {
+    public static void main (String[] args){
+        int total = 0;
+        for(int x : arr ){
+
+        }
+
+    }
+}
---
+++ src/src/arrays/Fill.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Fill {
+    public static void main (String[] args){
+        int res = 0;
+        for (int el : arr) {
+            res += el;
+
+        }
+
+    }
+}
